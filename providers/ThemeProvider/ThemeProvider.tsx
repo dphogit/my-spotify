2fa495879d85d37d53035d1834cf4140ae0915db
dphogit <deanp21@gmail.com>
2023-02-22 23:34:02 +1300

    feat: add jakarta font

--- providers/ThemeProvider/ThemeProvider.tsx
+++ providers/ThemeProvider/ThemeProvider.tsx
@@ -1,5 +1,54 @@
 import React from 'react';
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import localFont from '@next/font/local';
+
+const JakartaSans = localFont({
+  // Try to keep in order of directory listing please for easy checking
+  src: [
+    {
+      path: './fonts/PlusJakartaSans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PlusJakartaSans-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PlusJakartaSans-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PlusJakartaSans-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PlusJakartaSans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PlusJakartaSans-ExtraBoldItalic.woff2',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PlusJakartaSans-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PlusJakartaSans-SemiBoldItalic.woff2',
+      weight: '600',
+      style: 'italic',
+    },
+  ],
+});
+
+const fontFamily = `${JakartaSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`;
 
 interface ThemeProviderProps {
   children: React.ReactNode;
@@ -14,6 +63,8 @@ const ThemeProvider = ({ children, colorScheme, toggleColorScheme }: ThemeProvid
         theme={{
           colorScheme,
           primaryShade: 6,
+          fontFamily,
+          headings: { fontFamily, fontWeight: 800 },
           components: {
             Title: {
               styles: {
@@ -21,6 +72,7 @@ const ThemeProvider = ({ children, colorScheme, toggleColorScheme }: ThemeProvid
               },
             },
           },
+          black: '#191414',
           colors: {
             spotify: [
               '#b4e4bb',
