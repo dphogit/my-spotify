bbea05a4e357239ca104660ff40a74db7f8ecf47
dphogit <deanp21@gmail.com>
2023-02-23 13:54:02 +1300

    feat: add disclaimer and styles to unprotected home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
-import { Box, Button, Title } from '@mantine/core';
+import { Anchor, Box, Button, Center, Stack, Text, Title } from '@mantine/core';
 
 export default function HomePage() {
   return (
-    <Box>
-      <Title>My Spotify</Title>
-      <Button size="lg">Login With Spotify</Button>
-    </Box>
+    <Center h="90vh" pos="relative">
+      <Stack spacing="xl" align="center">
+        <Title ta="center">Visualize My Spotify</Title>
+        <Button size="xl">Login With Spotify</Button>
+      </Stack>
+      <Box pos="absolute" bottom={0}>
+        <Text>
+          This app is powered by the{' '}
+          <Anchor
+            href="https://developer.spotify.com/documentation/web-api/reference"
+            target="_blank"
+            rel="noreferrer external"
+          >
+            Spotify Web API
+          </Anchor>{' '}
+          and has no commercial affiliation with Spotify.
+        </Text>
+      </Box>
+    </Center>
   );
 }
