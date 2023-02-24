ac8bbbdff6cb0a9ed3f1e2ce945be27e4ee1b706
dphogit <deanp21@gmail.com>
2023-02-24 19:37:04 +1300

    style: profile card loading states

--- features/profile/components/ProfileCard/ProfileCard.tsx
+++ features/profile/components/ProfileCard/ProfileCard.tsx
@@ -1,10 +1,27 @@
-import { Box, Paper } from '@mantine/core';
+import { Box, Group, Paper, Skeleton, Stack } from '@mantine/core';
 
 const ProfileCard = () => {
+  // const { data, isLoading } = useGetMe();
+
   return (
-    <Box>
-      <Paper p="xl">Profile Card</Paper>
-    </Box>
+    <Paper p="xl">
+      <Group spacing="xl" position="apart">
+        <Box>
+          <Skeleton circle width={150} height={150} />
+        </Box>
+        <Stack spacing="md" sx={{ flex: 1, alignSelf: 'stretch' }}>
+          <Box>
+            <Skeleton height={36} />
+          </Box>
+          <Box>
+            <Skeleton height={36} />
+          </Box>
+          <Box>
+            <Skeleton height={36} />
+          </Box>
+        </Stack>
+      </Group>
+    </Paper>
   );
 };
 
