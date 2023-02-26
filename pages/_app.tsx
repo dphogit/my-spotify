3be2e1b939f369a61b515bee5fe1c786f3f9fbfe
dphogit <deanp21@gmail.com>
2023-02-26 21:21:25 +1300

    chore: update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>My Spotify</title>
+        <title>Visualize My Spotify</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/assets/icon.png" />
       </Head>
